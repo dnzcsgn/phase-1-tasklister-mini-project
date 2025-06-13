755a9c86c74cafd62a55ba848504d8787f4a5db2
dnzcsgn <dnzcsgndc@gmail.com>
2025-06-12 22:01:47 -0700

    updated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const taskInput = document.getElementById("new-task-description");
+    const task = taskInput.value;
+    buildToDo(task);
+  });
+
+  function buildToDo(task) {
+    const li = document.createElement("li");
+    li.textContent = task;
+    const taskList = document.getElementById("tasks");
+    taskList.appendChild(li);
+  }
 });
